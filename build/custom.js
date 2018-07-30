05138ce43f39eba544bbf617d574942d09e16e93
gmail007 <ankitdubeymail1@gmail.com>
2018-07-30 13:40:25 +0530

    add build

---
+++ build/custom.js
@@ -0,0 +1,12 @@
+// import $ from 'jquery';
+//
+// $(document).ready(function () {
+//
+//
+// $(window).load(function() {
+//     $('.flexslider').flexslider({
+//         animation: "slide"
+//     });
+// });
+//
+// });
